53f5373911b1736b34b46c78e445ffda19c95177
Taketo Wakabayashi <michaelowen628@gmail.com>
2019-03-17 23:10:00 +0900

    １ページだけを取得して出力

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("test")
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://db.netkeiba.com/horse/2013105880/'
+
+html = requests.get(url)
+soup = BeautifulSoup(html.content, 'html.parser')
+
+print(soup)
