d49a6084bb8e3bfed600dffbc3dd7560c310bb21
Taketo Wakabayashi <michaelowen628@gmail.com>
2019-03-20 00:09:39 +0900

    セレクタ戦闘中

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://db.netkeiba.com/horse/2013105880/'
+url = 'https://db.netkeiba.com/horse/2013105889/'
 
 html = requests.get(url)
 soup = BeautifulSoup(html.content, 'lxml')
@@ -11,29 +11,33 @@ horseID = url.replace("https://db.netkeiba.com/horse/","").replace("/","")
 print("馬ID：" + horseID)
 
 # 馬名
-horseName = soup.select("#db_main_box > div.db_head.fc > div.db_head_name.fc > div.horse_title > h1")
+horseName = soup.select_one("#db_main_box > div.db_head.fc > div.db_head_name.fc > div.horse_title > h1").string
 print("馬名：" + horseName)
 
 # 年齢
-horseAge = soup.select("#db_main_box > div.db_head.fc > div.db_head_name.fc > div.horse_title > p.txt_01")
+horseAge = soup.select_one("#db_main_box > div.db_head.fc > div.db_head_name.fc > div.horse_title > p.txt_01").string
 print("年齢：" + horseAge)
 
+
+#################
+#ここから先未クリア
+#################
 # 調教師
-horseTrainer = soup.select("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > table > tbody > tr:nth-child(2) > td > a")
+horseTrainer = soup.select_one("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > table > tbody > tr:nth-of-type(2) > td > a")
 print("調教師：" + horseTrainer)
 
 # 馬主
-horseOwner = soup.select("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > table > tbody > tr:nth-child(3) > td > a")
+horseOwner = soup.select_one("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > table > tbody > tr:nth-of-type(3) > td > a").string
 print("馬主：" + horseOwner)
 
 # 獲得総賞金
-horseTotalPrizeMoney = soup.select("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > table > tbody > tr:nth-child(7) > td")
+horseTotalPrizeMoney = soup.select_one("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > table > tbody > tr:nth-of-type(7) > td")
 print("獲得総賞金：" + horseTotalPrizeMoney)
 
 # 父馬
-horseFather = soup.select("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > div > dl > dd > table > tbody > tr:nth-child(1) > td:nth-child(1) > a")
+horseFather = soup.select_one("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > div > dl > dd > table > tbody > tr:nth-of-type(1) > td:nth-of-type(1) > a").string
 print("父馬：" + horseFather)
 
 # 母馬
-horseMother = soup.select("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > div > dl > dd > table > tbody > tr:nth-child(3) > td.b_fml > a")
+horseMother = soup.select_one("#db_main_box > div.db_main_deta > div > div.db_prof_area_02 > div > dl > dd > table > tbody > tr:nth-of-type(3) > td.b_fml > a").string
 print("母馬：" + horseMother)
