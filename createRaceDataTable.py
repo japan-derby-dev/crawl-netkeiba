1eb53aecc9d8442958de6898afd82d533afb6f9e
banbuu16 <ban.buu16@gmail.com>
2019-03-25 22:50:15 +0900

    再起処理実装

--- createRaceDataTable.py
+++ createRaceDataTable.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[179]:
+# In[203]:
 
+
+#作成日：2019/03/26
+#作成者：japan-Derby-Dev Project Member
+#利用範囲：
+# - 作成者が直接参画するプロジェクトの範囲に限る。
+# - 他のプロジェクトへの利用禁止
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,6 +16,7 @@ import pandas as pd
 import csv
 from urllib.parse import urljoin
 import os.path, time, re
+import datetime
 
 #テーブル情報からDataFrame生成する関数
 def getRaceResult(URL):
@@ -49,9 +56,12 @@ def getRaceResult(URL):
             data.append(link_split[-2])            
             df.loc[i] = data
             i = i + 1
+    #発走取りやめになった馬を削除
+    df = df[df['着順'] != '取']
+    df = df[df['着順'] != '中']
     #---------------------------------------------------------
     
-    #データ加工１：馬体重と増減を分ける---------------------------
+    #データ加工１：馬体重/増減を分ける---------------------------
     for idx, data in enumerate(df['馬体重']):
         data_list = data.split('(')
         data_list[1] = data_list[1][:-1]
@@ -59,12 +69,16 @@ def getRaceResult(URL):
         df.at[idx, '体重増減'] = data_list[1]
     #---------------------------------------------------------
     
-    #データ加工２：年齢と性別を分ける---------------------------
+    #データ加工２：年齢/性別を分ける---------------------------
     for idx, data in enumerate(df['性齢']):
         df.at[idx, '性齢'] = data[0]
         df.at[idx, '年齢'] = data[1]
     #---------------------------------------------------------
     
+    # 使えない列を削除
+    del_col = ['調教ﾀｲﾑ','厩舎ｺﾒﾝﾄ','備考','ﾀｲﾑ指数']
+    df = df.drop(del_col, axis=1)
+        
     return df
 
 
@@ -77,49 +91,62 @@ def getRaceHeader(URL):
     soup = BeautifulSoup(html.content, 'html.parser')
 
     #カラムの定義（後処理で変更される）
-    c_names = ['race_num','race_kind','race_type','race_info','date','detail','sex','syokin']
-    df = pd.DataFrame(columns=c_names)
+    c_names = ['race_num','race_kind','race_type','race_info']
+    df = pd.DataFrame(columns=c_names, )
 
     #レースヘッダーとなる情報を指定
     div = soup.findAll("div",{"class":CLS_NAME})[0]
     data_sets = div.findAll(['dt','h1','p'])
+
     row = []
     for i,data in enumerate(data_sets):      
         row_data = data.get_text()
+        row_data = row_data.replace(u'\n',u'')
+        row_data = row_data.replace(u'\xa0\xa0',u' ')
+        row_data = row_data.replace(u'\xa0/\xa0',u'/')
         row.append(row_data)
 
     df.loc[0] = row
-    
-    #データ加工処理１:race_typeを分割して付加---------------#
+
+
+    #データ加工処理１:race_typeを分割して付加-1 ---------------#
+    ts = df.at[0,'race_type'].split('/')
+    df['race_type'] = ts[0]
+    df['weather'] = ts[1][-1]
+    df['ground_condition'] = ts[2][-1]
+    df['dep_time'] = ts[3][4:]
+    #-----------------------------------------------------#
+
+    #データ加工処理１:race_typeを分割して付加-2 ---------------#
     t = df.at[0,'race_type']
     t_1 = t[:1]
     t = t[1:]
     tmp_t = t.split('m')
-    
+
     df['ground_type'] = t_1
-    df['race_distance'] = tmp_t[0]
-    df['direction'] = tmp_t[1]
-    
+    df['race_distance'] = tmp_t[0][1:]
+    df['direction'] = tmp_t[0][:1]
+
     #もともとのカラムを削除
     df = df.drop('race_type', axis=1)
     #-----------------------------------------------------#
-    
-    #データ加工処理２:race_typeを分割して付加---------------#
-    s = df.at[0, 'race_info']
-    tmp_s = s.split("/")
-    
-    df['weather'] = tmp_s[0]
-    df['ground_condition'] = tmp_s[1]    
-    #発走タイムは捨てる。
-    #もともとのカラムを削除
-    df = df.drop('race_info', axis=1)
-    #-----------------------------------------------------#
 
-    #データ加工処理３:dateから日付だけ取得--------------#
-    u = df.at[0, 'date']
-    df['date'] = u.split("(")[0]    
-    #-----------------------------------------------------#
-    
+    #データ加工処理２:race_infoを分割して付加 ---------------#
+    ts = df.at[0,'race_info'].split(' ')
+
+    for idx, data in enumerate(ts):
+        if len(ts[idx]) != 0:
+            if idx==0:
+                ts[idx] = ts[idx].replace('年','/')
+                ts[idx] = ts[idx].replace('月','/')
+                ts[idx] = ts[idx].replace('日','')
+                df.at[0, 'date'] = datetime.datetime.strptime(ts[idx], '%Y/%m/%d')
+            else :
+                df['race_info_'+str(idx)] = ts[idx]           
+
+    #もともとのカラム削除
+    df = df.drop('race_info', axis=1)    
+    #-----------------------------------------------------#    
     return df
 
 #ヘッダーDataFrameとレース結果DataFrameを横結合
@@ -129,28 +156,22 @@ def combineDataFrame(r_df, h_df, fid=1):
     df = pd.concat([r_df, h_df], axis=1)
     
     #csv出力
-    file_name = "raceTable_" + str(fid) + ".csv"
+    #file_name = "raceTable_" + str(fid) + ".csv"
     #df.to_csv(file_name)
     
     return df
 
-#確認用
-#url = "https://race.netkeiba.com/?pid=race&id=p201906020701&mode=top"
-#r_df = getRaceResult(url)
-#h_df = getRaceHeader(url)
-#df = combineDataFrame(r_df, h_df, 2)
-
 #開催レース一覧ページ（日別）からレースページのリンク配列を生成
 def getLinks(URL):
     #定数定義
-    CLS_NAME = "racename"
+    CLS_NAME = "race_top_data_info fc"
     
     html = requests.get(URL)
     soup = BeautifulSoup(html.content, 'html.parser')
 
     links = []
     #テーブルを指定->レコード取得
-    races = soup.find_all("div",{"class":CLS_NAME})
+    races = soup.find_all("dl",{"class":CLS_NAME})
    
     for race in races:
         race_link = race.find('a').get('href')
@@ -158,20 +179,81 @@ def getLinks(URL):
         
     return links
 
-#確認用----------------------------------------------------------
-url = 'https://race.netkeiba.com/?pid=race_list&id=p0316'
-links = getLinks(url)
-org_df = pd.DataFrame()
+#カレンダーからレースページURLを取得
+def createRaceLinkFromCalender(URL):    
+    CLS_NAME = "race_calendar"
 
-#取得したリンク配列を使ってcsvを連続生成
-for idx, url in enumerate(links):
-    r_df = getRaceResult(url)
-    h_df = getRaceHeader(url)
-    df = combineDataFrame(r_df, h_df, idx)
-    org_df = pd.concat([org_df,df])
+    html = requests.get(URL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    #テーブルを指定->レコード取得
+    calendar = soup.findAll("div",{"class":CLS_NAME})[0]
+    tds = calendar.find_all("td")
+    links = []
+    base = 'https://db.netkeiba.com/'
+    for td in tds:
+        race_link = td.find('a')
+        if race_link is not None:
+            race_link_url = race_link.get('href')
+            links.append(urljoin(base, race_link_url))
+
+    return links
+
+#カレンダーから次の月のカレンダーページを取得
+def createNextMonthLink(URL):
+    CLS_NAME_1 = "race_calendar"
+    CLS_NAME_2 = "next"
+
+    html = requests.get(URL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    #テーブルを指定->レコード取得
+    calendar = soup.findAll("div",{"class":CLS_NAME_1})[0]
+    li = calendar.findAll('li',{'class':CLS_NAME_2})[0]
+    next_month_link  = li.find_all('a')[-1].get('href')
+
+    next_month_url  = urljoin(base, next_month_link)
+    
+    return next_month_url
+
+
+
+#main処理---------------------------------------------------------------------
+#**********************************************************
+#ページリンクを辿って直近のレースまで再帰的にデータを取得
+#翌月のレースへ向かって処理を行う。
+#そのため、スタートしたいU過去日のURLを初期引数に与えること。
+#**********************************************************
+
+#初期URL
+n_mon = 'https://db.netkeiba.com/?pid=race_kaisai&syusai=10&date=20190105'
+
+merge_df = pd.DataFrame()
+while True:
+    r_links = createRaceLinkFromCalender(n_mon)
     time.sleep(1)
     
-org_df.reset_index()
-org_df.to_csv("race_merge.csv")
-#----------------------------------------------------------------
+    for r_link in r_links:
+        races = getLinks(r_link)
+        time.sleep(1)
+
+        #取得したリンク配列を使ってcsvを連続生成
+        for idx, url in enumerate(races):
+            #print("URL: ",str(url))
+            r_df = getRaceResult(url)
+            h_df = getRaceHeader(url)
+            df = combineDataFrame(r_df, h_df, idx)
+            merge_df = pd.concat([merge_df,df])
+            time.sleep(1)
+
+    time.sleep(1)
+    try:
+        n_mon = createNextMonthLink(r_links[0])
+        time.sleep(1)
+    except:
+        print("次月のカレンダーが・・・ない！？（処理を終了）")
+        merge_df = merge_df.reset_index()
+        merge_df.to_csv("race_merge.csv")
+        break
+#------------------------------------------------------------------------------
 
