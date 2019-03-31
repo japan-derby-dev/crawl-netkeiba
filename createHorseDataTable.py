230c68587e3bb1d2681066dad088e0611a6e7a1d
Taketo Wakabayashi <michaelowen628@gmail.com>
2019-03-31 21:42:33 +0900

    馬ページ巡回ロジック実装

--- createHorseDataTable.py
+++ createHorseDataTable.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from itertools import product
+from urllib.parse import urljoin
 import time
 
 # スクレイピングに利用するクラス名
@@ -106,10 +107,150 @@ def getHorseData(url):
     return horseinfodata_list, horseraceresultdata_list
 
 
+def formatHorseInfo(horseinfo_df):
+    # 基本情報データフレーム加工
+    horseinfo_df["horse_name"] = horseinfo_df["horse_name"].str.strip()
+    horseinfo_df["horse_age"] = horseinfo_df["horse_age"].str.strip()
+    horseinfo_df["horse_father"] = horseinfo_df["horse_father"].str.strip()
+    horseinfo_df["horse_paternalgrandfather"] = horseinfo_df["horse_paternalgrandfather"].str.strip()
+    horseinfo_df["horse_paternalgrandmother"] = horseinfo_df["horse_paternalgrandmother"].str.strip()
+    horseinfo_df["horse_mother"] = horseinfo_df["horse_mother"].str.strip()
+    horseinfo_df["horse_maternalgrandfather"] = horseinfo_df["horse_maternalgrandfather"].str.strip()
+    horseinfo_df["horse_maternalgrandmother"] = horseinfo_df["horse_maternalgrandmother"].str.strip()
+
+    return horseinfo_df
+
+
+def formatHorseRaceResult(horseraceresult_df):
+    # レース結果情報データフレーム加工
+    horseraceresult_df["レース名"] = horseraceresult_df["レース名"].str.split(
+        "/", expand=True)[2]
+    horseraceresult_df["コースタイプ"] = horseraceresult_df["距離"].str.get(0)
+    horseraceresult_df["距離"] = horseraceresult_df["距離"].str.strip("芝|ダ|障")
+    horseraceresult_df["通過平均"] = horseraceresult_df["通過"].str.split(
+        "-", expand=True).astype(float).mean(axis=1, skipna=True)
+    horseraceresult_df["ペース前半"] = horseraceresult_df["ペース"].str.split(
+        "-", expand=True)[0]
+    horseraceresult_df["ペース後半"] = horseraceresult_df["ペース"].str.split(
+        "-", expand=True)[1]
+    horseraceresult_df["馬体重増減"] = horseraceresult_df["馬体重"].str.split(
+        "(", expand=True)[1]
+    horseraceresult_df["馬体重増減"] = horseraceresult_df["馬体重増減"].str.strip(")")
+    horseraceresult_df["馬体重"] = horseraceresult_df["馬体重"].str.split(
+        "(", expand=True)[0]
+
+    return horseraceresult_df
+
+
+def getHourseLinksFromRacePage(URL):
+    CLS_NAME = "race_table_01 nk_tb_common"
+
+    html = requests.get(URL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    RaceResultTbl = soup.findAll(
+        'table', {'class': CLS_NAME})[0]
+    rows = RaceResultTbl.findAll('tr')
+
+    links = []
+    for row in rows:
+        index = 1
+        for cell in row.findAll('td'):
+            if index == 4:
+                links.append(cell.find('a').get('href'))
+            index += 1
+
+    return links
+
+
+def getRaceLinksFromEventDatePage(URL):
+    # 開催レース一覧ページ（日別）からレースページのリンク配列を生成
+    # 定数定義
+    CLS_NAME = "race_top_data_info fc"
+
+    html = requests.get(URL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    links = []
+    # テーブルを指定->レコード取得
+    races = soup.find_all("dl", {"class": CLS_NAME})
+
+    for race in races:
+        race_link = race.find('a').get('href')
+        links.append(urljoin(URL, race_link))
+
+    return links
+
+
+def getEventDateLinksFromCalender(URL):
+    # カレンダーからレースページURLを取得
+    CLS_NAME = "race_calendar"
+
+    html = requests.get(URL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    # テーブルを指定->レコード取得
+    calendar = soup.findAll("div", {"class": CLS_NAME})[0]
+    tds = calendar.find_all("td")
+    links = []
+    base = 'https://db.netkeiba.com/'
+    for td in tds:
+        race_link = td.find('a')
+        if race_link is not None:
+            race_link_url = race_link.get('href')
+            links.append(urljoin(base, race_link_url))
+    return links
+
+
+def createNextMonthLink(URL):
+    # カレンダーから次の月のカレンダーページを取得
+    CLS_NAME_1 = "race_calendar"
+    CLS_NAME_2 = "next"
+
+    html = requests.get(URL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    # テーブルを指定->レコード取得
+    calendar = soup.findAll("div", {"class": CLS_NAME_1})[0]
+    li = calendar.findAll('li', {'class': CLS_NAME_2})[0]
+    next_month_link = li.find_all('a')[-1].get('href')
+
+    next_month_url = urljoin(base, next_month_link)
+
+    return next_month_url
+
+
 ###########################################################
 # main
 ###########################################################
-url = 'https://db.netkeiba.com/horse/2016102227/'
+# 初期URL
+n_mon = 'https://db.netkeiba.com/?pid=race_kaisai&syusai=10&date=20190302'
+
+while True:
+    ed_links = getEventDateLinksFromCalender(n_mon)
+    time.sleep(1)
+
+    for ed_link in ed_links:
+        r_links = getRaceLinksFromEventDatePage(ed_link)
+        time.sleep(1)
+
+        for r_link in r_links:
+            h_links = getHourseLinksFromRacePage(r_link)
+            print(h_links)
+            print(len(h_links))
+
+    break
+'''
+    try:
+        n_mon = createNextMonthLink(r_links[0])
+        time.sleep(1)
+    except:
+        print("次月のカレンダーが・・・ない！？（処理を終了）")
+        merge_df = merge_df.reset_index(drop=True)
+        merge_df.to_csv("race_merge.csv")
+        break
+
+# 配列取得
 horseinfo_header, horseraceresult_header = getHorseHeader(url)
 horseinfo_data, horseraceresult_data = getHorseData(url)
 
@@ -118,34 +259,6 @@ horseinfo_df = pd.DataFrame([horseinfo_data], columns=horseinfo_header)
 horseraceresult_df = pd.DataFrame(
     horseraceresult_data, columns=horseraceresult_header)
 
-# データフレーム加工
-# 基本情報
-horseinfo_df["horse_name"] = horseinfo_df["horse_name"].str.strip()
-horseinfo_df["horse_age"] = horseinfo_df["horse_age"].str.strip()
-horseinfo_df["horse_father"] = horseinfo_df["horse_father"].str.strip()
-horseinfo_df["horse_paternalgrandfather"] = horseinfo_df["horse_paternalgrandfather"].str.strip()
-horseinfo_df["horse_paternalgrandmother"] = horseinfo_df["horse_paternalgrandmother"].str.strip()
-horseinfo_df["horse_mother"] = horseinfo_df["horse_mother"].str.strip()
-horseinfo_df["horse_maternalgrandfather"] = horseinfo_df["horse_maternalgrandfather"].str.strip()
-horseinfo_df["horse_maternalgrandmother"] = horseinfo_df["horse_maternalgrandmother"].str.strip()
-
-# レース結果情報
-horseraceresult_df["レース名"] = horseraceresult_df["レース名"].str.split(
-    "/", expand=True)[2]
-horseraceresult_df["コースタイプ"] = horseraceresult_df["距離"].str.get(0)
-horseraceresult_df["距離"] = horseraceresult_df["距離"].str.strip("芝|ダ|障")
-horseraceresult_df["通過平均"] = horseraceresult_df["通過"].str.split(
-    "-", expand=True).astype(float).mean(axis=1, skipna=True)
-horseraceresult_df["ペース前半"] = horseraceresult_df["ペース"].str.split(
-    "-", expand=True)[0]
-horseraceresult_df["ペース後半"] = horseraceresult_df["ペース"].str.split(
-    "-", expand=True)[1]
-horseraceresult_df["馬体重増減"] = horseraceresult_df["馬体重"].str.split(
-    "(", expand=True)[1]
-horseraceresult_df["馬体重増減"] = horseraceresult_df["馬体重増減"].str.strip(")")
-horseraceresult_df["馬体重"] = horseraceresult_df["馬体重"].str.split(
-    "(", expand=True)[0]
-
 # 加工状態確認
 print(horseinfo_df)
 print(horseraceresult_df)
@@ -153,3 +266,4 @@ print(horseraceresult_df)
 # データフレームCSV出力
 horseinfo_df.to_csv("horse.csv")
 horseraceresult_df.to_csv("horseraceresult.csv")
+'''
